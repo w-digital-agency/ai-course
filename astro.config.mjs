f21928453a236190b888aa9b8b3854d7b0245249
twl-dev <hi@wingleung.me>
2025-07-02 00:40:25 +0800

    update astro page configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://w-digital-agency.github.io',
+  base: '/wdigital-astro',
   integrations: [tailwind()],
 });
+
